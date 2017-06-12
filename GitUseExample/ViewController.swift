caf811418c2aa68ec48c708a874734f26269beae
Joachim Vetter <JoachimVetter@joachims-mbp>
2017-06-12 21:43:44 +0200

    Just added another Print...nothign else!

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     @IBAction func buttonClicked(_ sender: UIButton) {
         print("This is still a good Git Tutorial!")
         print("Hallo?")
+        print("Yeah")
         myLabel.text = "Oops, text has been changed now."
     }
 }
