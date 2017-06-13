b4d9051bb25295e0878868ab6cffd9b7ecc98c63
Joachim Vetter <JoachimVetter@joachims-mbp>
2017-06-13 09:27:42 +0200

    Finishing Touches 1

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -19,8 +19,7 @@ class ViewController: UIViewController {
 
     @IBAction func buttonClicked(_ sender: UIButton) {
         print("This is still a good Git Tutorial!")
-        print("Hallo?")
-        print("Yeah")
+        print("Finished right now")
         myLabel.text = "Oops, text has been changed now."
     }
     @IBAction func calculate(_ sender: UIButton) {
